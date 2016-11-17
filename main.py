f6d1cdf7a34f46ec1d5d722783eb96940972f9d5
mayo-ya <mayo.yamasaki@gmail.com>
2016-11-18 00:35:47 +0900

    [Add] main func

---
+++ main.py
@@ -0,0 +1,99 @@
+import argparse
+from logging import getLogger
+
+import numpy as np
+from gensim.models.word2vec import Word2Vec
+from sklearn.cluster import KMeans
+from sklearn.externals import joblib
+
+
+logger = getLogger(__name__)
+
+
+def make_dataset(model, sample=None):
+
+    if sample is not None:
+        logger.info('start to create {} samples dataset.'.format(sample))
+        V = np.random.choice(model.index2word, sample)
+    else:
+        V = model.index2word
+    X = np.zeros((len(V), model.vector_size))
+
+    for index, word in enumerate(V):
+        X[index, :] += model[word]
+    return X
+
+
+def train(X, K):
+    logger.info('start to fiting KMeans with {} classs.'.format(K))
+    classifier = KMeans(n_clusters=K, random_state=0)
+    classifier.fit(X)
+    return classifier
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='Python Word2Vec Cluster')
+
+    parser.add_argument('model',
+                        action='store',
+                        help='Name of word2vec binary modelfile.')
+
+    parser.add_argument('-o', '--out',
+                        action='store',
+                        default='model.pkl',
+                        help='Set output filename.')
+
+    parser.add_argument('-k', '--K',
+                        action='store',
+                        type=int,
+                        default=500,
+                        help='Num of classes on KMeans.')
+
+    parser.add_argument('-s', '--sampling-size',
+                        action='store',
+                        type=int,
+                        default=None,
+                        help='Num of sumpling size to use training dataset.')
+
+    parser.add_argument('-p', '--pre-trained-model',
+                        action='store',
+                        default=None,
+                        help='Use pre-trained KMeans Model.')
+
+    parser.add_argument('-w', '--words-to-pred',
+                        action='store',
+                        nargs='+',
+                        type=str,
+                        default=None,
+                        help='List of word to predict.')
+
+    args = parser.parse_args()
+
+    model = Word2Vec.load_word2vec_format(args.model, binary=True)
+
+    if not args.pre_trained_model:
+        X = make_dataset(model, sample=args.sampling_size)
+        classifier = train(X, args.K)
+        joblib.dump(classifier, args.out)
+    else:
+        classifier = joblib.load(args.pre_trained_model)
+
+    if args.words_to_pred:
+
+        X = [model[word] for word in args.words_to_pred if word in model]
+        classes = classifier.predict(X)
+
+        result = []
+        i = 0
+        for word in args.words_to_pred:
+            if word in model:
+                result.append(str(classes[i]))
+                i += 1
+            else:
+                result.append(str(-1))
+        print(' '.join(result))
+
+
+if __name__ == '__main__':
+    main()
